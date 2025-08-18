a49df6169f695d76d9b6f41ac5092b0cfe1429a3
DarkCovenant <zandro.narvaza@gmail.com>
2025-08-18 22:33:17 +0800

    fix: Update registration URL path to remove 'accounts/' prefix

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import LoginView, LogoutView, PasswordChangeView, RegisterView
 
 urlpatterns = [
-    path("accounts/register/", RegisterView.as_view(), name="register"),
+    path("register/", RegisterView.as_view(), name="register"),
     path('login/', LoginView.as_view(), name='api-login'),
     path('logout/', LogoutView.as_view(), name='api-logout'),
     path('password-change/', PasswordChangeView.as_view(), name='api-password-change'),
