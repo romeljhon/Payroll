a87b72b9b21ffe586afc16b8718e4bd07eec07fe
DarkCovenant <zandro.narvaza@gmail.com>
2025-08-23 00:52:52 +0800

    feat: import time logs expose api

--- backend/config/api_urls.py
+++ backend/config/api_urls.py
@@ -4,9 +4,10 @@ from rest_framework.routers import DefaultRouter
 
 from organization.views import BusinessViewSet, BranchViewSet
 from employees.views import EmployeeViewSet
-from payroll.views import SalaryComponentViewSet, SalaryStructureViewSet, PayrollCycleViewSet, PayrollRecordViewSet
+from payroll.views import SalaryComponentViewSet, SalaryStructureViewSet, PayrollCycleViewSet, PayrollRecordViewSet, PayrollPolicyViewSet
 from positions.views import PositionViewSet
 from timekeeping.views import TimeLogViewSet, HolidayViewSet
+from timekeeping.views import TimeLogImportView
 from email_sender.views import SendSinglePayslipView
 
 router = DefaultRouter()
@@ -19,11 +20,13 @@ router.register('structure', SalaryStructureViewSet)
 router.register('timekeeping', TimeLogViewSet)
 router.register('holidays', HolidayViewSet)
 router.register('payrollcycle', PayrollCycleViewSet)
+router.register('policy', PayrollPolicyViewSet)
 router.register('records', PayrollRecordViewSet)
 
 urlpatterns = [
     # Non-ViewSet endpoints go here:
     path('email/send-single-payslip/', SendSinglePayslipView.as_view(), name='send-single-payslip'),
+    path('timelogs/import/', TimeLogImportView.as_view(), name='timelog-import'),
 ]
 
 # Include router-generated endpoints
